f3fafffe43ba8b31688aa372423c5c3601b079ba
Sangmin <Sangmin@DESKTOP-5ADAB19>
2020-01-07 10:44:09 +0900

    컨트롤러 수정, 마켓 DTO 추가, 제품 관련 커맨드 추가

---
+++ src/beans/MarketDTO.java
@@ -0,0 +1,56 @@
+package beans;
+
+public class MarketDTO {
+	private int mk_uid;
+	private String mk_name;
+	private String mk_insta;
+	private String mk_logo;
+	
+	//Constructor
+	MarketDTO(){}
+
+	public MarketDTO(int mk_uid, String mk_name, String mk_insta, String mk_logo) {
+		super();
+		this.mk_uid = mk_uid;
+		this.mk_name = mk_name;
+		this.mk_insta = mk_insta;
+		this.mk_logo = mk_logo;
+	}
+	
+	
+	//Getter and Setter
+	public int getMk_uid() {
+		return mk_uid;
+	}
+
+	public void setMk_uid(int mk_uid) {
+		this.mk_uid = mk_uid;
+	}
+
+	public String getMk_name() {
+		return mk_name;
+	}
+
+	public void setMk_name(String mk_name) {
+		this.mk_name = mk_name;
+	}
+
+	public String getMk_insta() {
+		return mk_insta;
+	}
+
+	public void setMk_insta(String mk_insta) {
+		this.mk_insta = mk_insta;
+	}
+
+	public String getMk_logo() {
+		return mk_logo;
+	}
+
+	public void setMk_logo(String mk_logo) {
+		this.mk_logo = mk_logo;
+	}
+	
+	
+	
+}
