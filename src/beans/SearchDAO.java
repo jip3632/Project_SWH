756584c6a6fde6958548b277fe86157c1d90c9e9
GB <GB@H19>
2020-01-08 14:48:37 +0900

    searchDAO 수정

--- src/beans/SearchDAO.java
+++ src/beans/SearchDAO.java
@@ -266,10 +266,25 @@ public class SearchDAO {
 	}
 	
 
-	public ProductInStoreDTO[] createArrayProductInStore(ResultSet rs) throws SQLException {
-		ArrayList<ProductInStoreDTO> list = new ArrayList<ProductInStoreDTO>();
+	public ProductInStoreDTO[] createArrayProductEveryStore(ResultSet rs) throws SQLException {
+		ArrayList<ProductEveryStoreDTO> list = new ArrayList<ProductEveryStoreDTO>();
 		while(rs.next()) {
 			int st_uid = rs.getInt("st_uid");
+			int mb_uid	= rs.getInt("mb_uid");
+			String st_name = rs.getString("st_name");
+			String st_address = rs.getString("st_address");
+			String st_contact = rs.getString("st_contact");
+			String st_description = rs.getString("st_description");
+			if(st_description == null) st_description = "";
+			double st_rating = rs.getDouble("st_rating");
+			String st_img = rs.getString("st_img");
+			if(st_img == null) st_img = "";
+			int st_valid_key = rs.getInt("st_valid_key");
+			String st_valid_img = rs.getString("st_valid_img");
+			String st_latitude = rs.getString("st_latitude");
+			String st_longitude = rs.getString("st_longitude");
+			double st_dist = 0;
+			String st_hours = rs.getString("st_hours");
 			int inv_uid = rs.getInt("inv_uid");
 			int inv_quantity = rs.getInt("inv_quantity");
 			int inv_price = rs.getInt("inv_price");
@@ -284,7 +299,7 @@ public class SearchDAO {
 			String mk_name = rs.getString("mk_name");
 			String mk_insta = rs.getString("mk_insta");
 			String mk_logo = rs.getString("mk_logo");
-			ProductInStoreDTO dto = new ProductInStoreDTO(st_uid,inv_uid,inv_quantity, inv_price, inv_volume, pd_uid, pd_name, pd_description, pd_img, mk_uid, mk_name, mk_insta, mk_logo);		
+			ProductEveryStoreDTO dto = new ProductEveryStoreDTO(st_uid, mb_uid, st_name, st_address, st_contact, st_description, st_rating, st_img, st_valid_key, st_valid_img, st_latitude, st_longitude, st_dist, st_hours, inv_uid ,inv_quantity, inv_price, inv_volume, pd_uid, pd_name, pd_description, pd_img, mk_uid, mk_name, mk_insta, mk_logo);		
 			list.add(dto);
 		}
 		ProductInStoreDTO[] arr = new ProductInStoreDTO[list.size()];
@@ -318,7 +333,7 @@ public class SearchDAO {
 			pstmt = conn.prepareStatement(D.SQL_SELECT_STORE_BY_PRODUCT); // query
 			pstmt.setInt(1, pd_uid);
 			rs = pstmt.executeQuery();
-			arr = createArrayProductInStore(rs);
+			arr = createArrayProductEveryStore(rs);
 		} finally {
 			close();
 		}
